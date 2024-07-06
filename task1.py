df9bde86a25a91ad16a8d944da555237a23db466
Parvathy2004 <130462396+Parvathy2004@users.noreply.github.com>
2024-07-06 19:16:50 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 from tkinter import messagebox
 
+#main window
 root = tk.Tk()
-#root.geometry("400x250")
 root.title("Welcome to my app")
 
+#main frame
 mainframe = tk.Frame(root, padx=20, pady=20, bg="white")
 mainframe.pack(pady=20, padx=20, expand=True, fill=tk.BOTH)  # Expand to fill available space
 
-
+#variable for main label
 text_var = tk.StringVar()
 text_var.set("Registration Form")
 
@@ -28,27 +29,30 @@ label = tk.Label(mainframe,
                  )
 label.pack()
 
+#sub frame within main frame
 sub_frame = tk.Frame(mainframe, bg="white")
 sub_frame.pack(anchor=tk.CENTER)
 
+#variables for name,email,age and gender 
 name_var = tk.StringVar()
 email_var = tk.StringVar()
 age_var = tk.StringVar()
 gen_var = tk.StringVar(root,"1")
 
-
+#function called when clicked on entry field
 def on_click(var,entry,placeholder):
     if var.get() == placeholder:
         var.set("")
         entry.configure(state="normal")
 
 
+#function called when clicked on other entry fields
 def on_focusout(var,entry,placeholder):
     if var.get() == "":
         entry.insert(0, placeholder)
         entry.configure(state="disabled")
 
-
+#function to reset values after clicking submit button
 def reset_values():
     name_entry.insert(0, "Enter your name")
     name_entry.configure(state="disabled")
@@ -59,6 +63,7 @@ def reset_values():
     gen_var.set("1")
     
 
+#function called when submit button is clicked
 def submit():
     name = name_var.get()
     email = email_var.get()
@@ -80,13 +85,14 @@ def submit():
     reset_values()
 
 
+#label and entry for all fields
 name_label = tk.Label(sub_frame, text="Username", font=("Times New Roman", 10, "bold"), bg="white")
 name_entry = tk.Entry(sub_frame, textvariable = name_var, font=('calibre',10,'normal'), bg="lightgrey", width=40)
 name_placeholder = "Enter your name"
-name_entry.insert(0, name_placeholder)
-name_entry.configure(state="disabled")
-name_entry.bind("<Button-1>", lambda event: on_click(name_var,name_entry,name_placeholder))
-name_entry.bind("<FocusOut>", lambda event: on_focusout(name_var,name_entry,name_placeholder))
+name_entry.insert(0, name_placeholder)  #initial text displayed on entry field before clicking
+name_entry.configure(state="disabled")  #initial state as disabled
+name_entry.bind("<Button-1>", lambda event: on_click(name_var,name_entry,name_placeholder))  #event happening on right click on entry
+name_entry.bind("<FocusOut>", lambda event: on_focusout(name_var,name_entry,name_placeholder))  #event happening on focus out
 
 email_label = tk.Label(sub_frame, text="Email Id", font=("Times New Roman", 10, "bold"), bg="white")
 email_entry = tk.Entry(sub_frame, textvariable = email_var, font=('calibre',10,'normal'), bg="lightgrey", width=40)
@@ -107,12 +113,14 @@ age_entry.bind("<FocusOut>", lambda event: on_focusout(age_var,age_entry,age_pla
 
 gen_label = tk.Label(sub_frame, text="Gender", font=("Times New Roman", 10, "bold"), bg="white")
 
+#radio buttons for different options for gender
 male_radio = tk.Radiobutton(sub_frame, text="Male", variable=gen_var, value="Male", bg="white")
 female_radio = tk.Radiobutton(sub_frame, text="Female", variable=gen_var, value="Female", bg="white")
 other_radio = tk.Radiobutton(sub_frame, text="Other", variable=gen_var, value="Other", bg="white")
 
-sub_btn = tk.Button(mainframe, text="Submit", command= submit)
+sub_btn = tk.Button(mainframe, text="Submit", command= submit)  #submit button
 
+#placing all widgets in window
 name_label.grid(row=0,column=0,pady=10, padx=10)
 name_entry.grid(row=0, column=1)
 
